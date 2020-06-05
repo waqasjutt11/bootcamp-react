5d9446742fe77aa472ae5ae3a6005a4fac33e706
waqasjutt11 <54747067+waqasjutt11@users.noreply.github.com>
2020-06-05 23:14:14 +0500

    update by waqas

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <br />
+          UPdated by Waqas Hussain
         </p>
         <a
           className="App-link"
