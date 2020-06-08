50ab7f95ba46dfc081bc90bb8abacbc831124a43
waqasjutt11 <54747067+waqasjutt11@users.noreply.github.com>
2020-06-08 19:00:32 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,25 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          <br />
-          UPdated by Waqas Hussain
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+          
   );
 }
 
